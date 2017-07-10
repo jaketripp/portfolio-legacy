d0dac5391ac5cd3f81f8445b8de31862455aa721
Jake Tripp <shmogens@gmail.com>
2017-07-09 22:53:36 -0500

    fix typo in footer, make FA async, fix wrong cascade because async

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
+// handle card dimmer
 $('.special.cards .image').dimmer({
   on: 'hover'
 });
 
+// copy to clipboard
 var clipboard = new Clipboard('#copy');
 
+// popup for copy to clipboard
 $('#copy').popup({on: 'manual'});
 
 $('#copy').on('click', function() {
     $('#copy').popup('show');
-    // hide after 1 second
     setTimeout(function() {
         $('#copy').popup('hide');
     }, 1000)
-    
 });
