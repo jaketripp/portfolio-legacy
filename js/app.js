8b3e7b2935754e1d0f8efc5c7654684aa738c19c
Jake Tripp <shmogens@gmail.com>
2017-07-09 21:13:27 -0500

    add README buttons, change resume and copy buttons to same style as contact buttons, add clipboard.js, change picture to more orange version, update color scheme

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $('.special.cards .image').dimmer({
   on: 'hover'
 });
+
+var clipboard = new Clipboard('#copy');
+
+$('#copy').popup({on: 'manual'});
+
+$('#copy').on('click', function() {
+    $('#copy').popup('show');
+    // hide after 1 second
+    setTimeout(function() {
+        $('#copy').popup('hide');
+    }, 1000)
+    
+});
