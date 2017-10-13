6fe97a2c20a55fba1187aa5a98e0dbc5cbdc421d
Jake Tripp <shmogens@gmail.com>
2017-10-13 15:34:30 -0500

    add animation to scroll down, add a chevron to scroll up

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 // handle card dimmer
 $('.special.cards .image').dimmer({
-  on: 'hover'
+	on: 'hover'
 });
 
 // copy to clipboard
 var clipboard = new Clipboard('#copy');
 
 // popup for copy to clipboard
-$('#copy').popup({on: 'manual'});
+$('#copy').popup({ on: 'manual' });
 
 $('#copy').on('click', function() {
-    $('#copy').popup('show');
-    setTimeout(function() {
-        $('#copy').popup('hide');
-    }, 1000)
+	$('#copy').popup('show');
+	setTimeout(function() {
+		$('#copy').popup('hide');
+	}, 1000);
+});
+
+$('#contactButton button').click(function(e) {
+    event.preventDefault();
+	var target = $("#contact").offset().top;
+	$('html, body').animate({ scrollTop: target }, 'slow');
+});
+
+$('#up').click(function(e) {
+    event.preventDefault();
+	$('html, body').animate({ scrollTop: 0 }, 'slow');
 });
