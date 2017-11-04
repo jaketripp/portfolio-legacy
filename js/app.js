d022a29a7ff65ae2b87f3b82a346aebf7b1dad94
Jake Tripp <shmogens@gmail.com>
2017-11-04 00:39:11 -0500

    add email form, add form validation (semantic has it built ingit add .).

--- js/app.js
+++ js/app.js
@@ -17,24 +17,68 @@ $('#copy').on('click', function() {
 });
 
 $('#contactButton button').click(function(e) {
-    e.preventDefault();
+	e.preventDefault();
 	var target = $("#contact").offset().top;
 	$('html, body').animate({ scrollTop: target }, 'slow');
 });
 
 $('#up').click(function(e) {
-    e.preventDefault();
+	e.preventDefault();
 	$('html, body').animate({ scrollTop: 0 }, 'slow');
 });
 
 $('#context1 .menu .item')
-  .tab({
-    context: $('#context1')
-  })
-;
+	.tab({
+		context: $('#context1')
+	});
 $('#context2 .menu .item')
-  .tab({
-    // special keyword works same as above
-    context: 'parent'
-  })
-;
+	.tab({
+		// special keyword works same as above
+		context: 'parent'
+	});
+
+$('.ui.form')
+	.form({
+		fields: {
+			name: {
+				identifier: 'name',
+				rules: [{
+					type: 'empty',
+					prompt: 'Please enter your name'
+				}]
+			},
+			company: {
+				identifier: 'company',
+				rules: [{
+					type: 'empty',
+					prompt: 'Please enter your current company'
+				}]
+			},
+			email: {
+				identifier: 'email',
+				rules: [{
+					type: 'email',
+					prompt: 'Please enter a valid email address'
+				}]
+			},
+			phone: {
+				identifier: 'phone',
+				rules: [
+				{
+					type: 'minLength[7]',
+					prompt: 'Please use a minimum of 7 numbers in your phone number'					
+				},
+				{
+					type: 'number',
+					prompt: 'Please use only numerical digits (0-9) in your phone number'
+				}]
+			},
+			message: {
+				identifier: 'message',
+				rules: [{
+						type: 'empty',
+						prompt: "Please leave a brief message explaining what project you'd like to work together on" 
+				}]
+			}
+		}
+	});
